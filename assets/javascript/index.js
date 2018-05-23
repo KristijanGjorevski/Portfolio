8c17e308f22f148659ef5da680a6692f68a9ee42
Kristijan Gjorevski <kikogjorevski@gmail.com>
2018-05-23 23:12:53 +0200

    updated

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,3 +3,25 @@ const introContent = document.querySelector(".main_content");
 window.setTimeout(() => {
     introContent.style.display = "flex";
 }, 970);
+
+
+const projects = document.querySelectorAll("#project-template");
+
+projects.forEach(element => {
+    element.addEventListener("mouseover",enterState);
+    element.addEventListener("mouseout",leaveState);
+});
+
+function enterState() {
+
+    //console.log(this);
+    this.style.backgroundColor = "white";
+    this.childNodes[1].style.color = "black";
+}
+
+function leaveState() {
+
+    this.style.backgroundColor = "transparent";
+    this.childNodes[1].style.color = "white";
+    
+}
